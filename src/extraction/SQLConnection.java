068bfa77b6025420e06412e64af6c5efc8922075
niu <408620775@qq.com>
2016-10-24 16:59:53 +0800

    change the SQLConnection.java because of adding the properties file

--- src/extraction/SQLConnection.java
+++ src/extraction/SQLConnection.java
@@ -1,45 +1,60 @@
 package extraction;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
+import java.util.Properties;
+
 /**
- * 数据库连接类.
- * 1.通过构造函数选择需要连接的数据库并实现与数据库的连接
- * 2.通过返回stmt,使调用SQLConnection的类能够执行sql语句
+ * 数据库连接类. 1.通过构造函数选择需要连接的数据库并实现与数据库的连接 2.通过返回stmt,使调用SQLConnection的类能够执行sql语句
+ * 
  * @author niu
  *
  */
 public class SQLConnection {
-	String database;
 	Connection conn = null;
-	Statement stmt=null;
+	Statement stmt = null;
+	private String drivername, databasename, userName, password;
 
-	SQLConnection(String database){
-		this.database=database;
-		connect();
-	}
-	public void connect() {
-		String url = "jdbc:mysql://localhost:3306/"+database+"?"
-				+ "user=root&password=password&useUnicode=true&CharacterEncoding=UTF8";
-		
+	public SQLConnection(String propName) {
+		File propsfile = new File(propName);
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
+			FileInputStream fis = new FileInputStream(propsfile);
+			Properties prop = new Properties();
+			prop.load(fis);
+			drivername=prop.getProperty("Driver");
+			databasename=prop.getProperty("URL");
+			userName=prop.getProperty("UserName");
+			password=prop.getProperty("Password");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	
+	}
+
+	public void connect() {
+		try {
+			Class.forName(drivername);
 			System.out.println("成功加载mysql驱动");
-			conn = DriverManager.getConnection(url);
+			conn = DriverManager.getConnection(databasename,userName,password);
 			stmt = conn.createStatement();
 		} catch (Exception e) {
 			System.out.println("MySQL操作错误");
 			e.printStackTrace();
 		}
 	}
+
 	public Statement getStmt() {
 		return stmt;
 	}
+
 	public String getDatabase() {
-		return database;
-	}
-	public void setDatabase(String database) {
-		this.database = database;
+		return databasename;
 	}
 }
